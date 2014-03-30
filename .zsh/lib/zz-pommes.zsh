689fdd9c4b2ced1966d07e1bd625f4c6309a7875
Tim Pommerening <tim@v3-1191.vxen.de>
2014-03-30 20:42:08 +0200

    better ls

--- .zsh/lib/zz-pommes.zsh
+++ .zsh/lib/zz-pommes.zsh
@@ -4,3 +4,11 @@
 # Prompt
 PS1='%B%{$fg[blue]%}%#%  %{$fg[green]%}%n@%{$fg[cyan]%}%m %{$fg[yellow]%}%~%{$fg[white]%}${vcs_info_msg_0_}
 %{$fg[red]%}$ %{$reset_color%}'
+
+# enable color support of ls and also add handy aliases
+if [ "$TERM" != "dumb" ]; then
+    eval "`dircolors -b`"
+    alias ls='ls --color=auto'
+    #alias dir='ls --color=auto --format=vertical'
+    #alias vdir='ls --color=auto --format=long'
+fi
