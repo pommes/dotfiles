c748ece18bce6d52f83b5261a6d5671b030e1aee
Tim Pommerening <tim@tyranus.de>
2014-03-30 20:34:17 +0200

    better prompt

--- .zsh/lib/zz-pommes.zsh
+++ .zsh/lib/zz-pommes.zsh
@@ -2,4 +2,5 @@
 #autoload -U colors && colors
 
 # Prompt
-PS1='%B%{$fg[green]%}%n@%{$fg[cyan]%}%m %{$fg[yellow]%}%~%{$fg[white]%}${vcs_info_msg_0_}$ %{$reset_color%}'
+PS1='%B%{$fg[blue]%}%#%  %{$fg[green]%}%n@%{$fg[cyan]%}%m %{$fg[yellow]%}%~%{$fg[white]%}${vcs_info_msg_0_}
+%{$fg[red]%}$ %{$reset_color%}'
