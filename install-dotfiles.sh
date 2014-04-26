2fbcdbacfe82569ead0d5487198e39077ca2680c
root <root@coruscant.(none)>
2014-04-26 13:19:41 +0000

    .vim und .vimrc

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 dir=/bu/skel/pommes-dotfiles
+timestamp=`date +"%Y%m%d-%H%M%S"`
 
 cd
 ln -s ${dir}/.zsh .zsh
 ln -s ${dir}/.zshrc .zshrc
 ln -s ${dir}/.zlogin .zlogin
-
+if [ -d ${dir}/.vim ]; then mv ${dir}/.vim ${dir}/.vim_${timestamp}; fi
+ln -s ${dir}/.vim .vim
+if [ -d ${dir}/.vimrc ]; then mv ${dir}/.vimrc ${dir}/.vimrc_${timestamp}; fi
+ln -s ${dir}/.vimrc .vimrc
